5ad74b896dc69ac1d89d56e4e30616fb96021665
denis-petrov <den11032001@mail.ru>
2021-04-07 00:32:37 +0300

    refactoring

--- labs/lab4/task1/task1/stdafx.h
+++ labs/lab4/task1/task1/stdafx.h
@@ -11,4 +11,9 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+const int DEFAULT_DENSITY = 1;
+const int DEFAULT_RADIUS = 1;
+const int DEFAULT_WIDTH = 1;
+const int DEFAULT_HEIGTH = 1;
+const int DEFAULT_DEPTH = 1;
 const int DEFAULT_PRECISION = 2;
